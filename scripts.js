453644b4e2b68b8df909dafb2b2c6e5238d4ccd2
Paula <mv.paula@gmail.com>
2025-02-25 10:56:21 +0100

    mi solucion al kata

--- scripts.js
+++ scripts.js
@@ -45,22 +45,37 @@ function imprimePregunta(pregunta) {
 
 function imprimeTitulo(pregunta) {
   // Put your code here
+  return "<p>" + pregunta.titulo + "</p>";
 }
 
 function imprimeTodasLasRespuestas(pregunta) {
   // Put your code here
+  let respuestas = "";
+  //hay 5 respuestas en el enunciado así que el bucle se iterará 5 veces
+  for (let i = 0; i < 5; i++) {
+    respuestas += imprimeUnaRespuesta(i); //usaremos la funcion de imprimir una 
+  }
+  return respuestas;
 }
 
 function imprimeUnaRespuesta(respuesta) {
   // Put your code here
+  //usamos las funciones que declaramos despues
+  return imprimeLabel(respuesta) + imprimeInput(respuesta);
 }
 
+
+//output será <label for="william-shatner">William Shatner</label>
 function imprimeLabel(respuesta) {
   // Put your code here
+  //hay que mirar el output esperado y coger cada "porcion" del codigo, enlazando con ${}
+  return `<label for=${pregunta.respuestas[respuesta].id}>${pregunta.respuestas[respuesta].label}</label>`;
 }
 
+//output será <input id="blablabla" name="actor" type="radio" value="blablabla">
 function imprimeInput(respuesta) {
   // Put your code here
-}
+  return `<input id=${pregunta.respuestas[respuesta].id} name=${pregunta.respuestas[respuesta].name} type="radio" value=${pregunta.respuestas[respuesta].value}>`;
+  }
 
 document.getElementById("contenedorPreguntas").innerHTML = imprimePregunta(pregunta);
